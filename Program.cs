8c66a913977ab41baba3d92fda3ab639f2badf26
camicamicam <camilamata04@gmail.com>
2025-02-13 10:30:11 -0600

    Prueba 1

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Helloooo cristina");
+Console.WriteLine("Aqui empezamos creo");
